7c0c03e0449ff2842500ccbe26881b7123d102dc
Kanna-25 <Kanna-25@users.noreply.github.com>
2026-05-30 23:25:37 +0900

    fix:デプロイ用PORT修正

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -36,7 +36,7 @@ app.use((err: any, req: any, res: any, next: any) => {
   });
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
